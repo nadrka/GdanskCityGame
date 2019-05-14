eb49249bcfff060f70f0a06a82e749c6556d61b0
Gustaw Ohler <gustaw@ohler.pl>
2019-05-14 20:00:50 +0200

    added localization functions

--- CityGame/Map/MapView.swift
+++ CityGame/Map/MapView.swift
@@ -1,11 +1,14 @@
 import UIKit
 import MapKit
 import SnapKit
+import CoreLocation
 
 class MapView: UIView, CLLocationManagerDelegate {
     private var mapView: MKMapView!
     private var locationButton: UIImageView!
     private var shouldTrackUserLocation = true
+    private var locationManager = CLLocationManager()
+    private var regionInMeters: Double = 5000
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -20,6 +23,7 @@ class MapView: UIView, CLLocationManagerDelegate {
     private func setupMainView() {
         setupMapView()
         setupLocationButton()
+        checkLocationServices()
     }
 
     func setupMapView() {
@@ -43,9 +47,22 @@ class MapView: UIView, CLLocationManagerDelegate {
         locationButton.addGestureRecognizer(tapGesture)
         mapView.addSubview(locationButton)
     }
+    
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
 
     @objc func onLocationButtonTapped() {
         print("location button tappped")
+        zoomInLocation()
+    }
+    
+    func zoomInLocation(){
+        if let location = locationManager.location?.coordinate{
+            let region = MKCoordinateRegion(center: location, latitudinalMeters:regionInMeters, longitudinalMeters:regionInMeters)
+            mapView.setRegion(mapView.regionThatFits(region), animated: true)
+        }
     }
 
     @objc func didScrollMap() {
@@ -61,6 +78,46 @@ class MapView: UIView, CLLocationManagerDelegate {
             make.right.equalToSuperview().inset(15.sketchWidth)
             make.top.equalTo(self.snp_topMargin).inset(50.sketchHeight)
         }
+    }
+    
+    func checkLocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            //show alert about turning on location
+        }
+    }
+    
+    func checkLocationAuthorization(){
+        switch CLLocationManager.authorizationStatus(){
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            zoomInLocation()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            //SHOW ALERT
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            //SHOW ALERT
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
     }
 }
 
