4b9a38c98ee88c6a3c915abc514c9a227014e000
TaubeKryst <38167239+TaubeKryst@users.noreply.github.com>
2019-06-10 19:49:46 +0200

    Add files via upload
    Add REST request to login

--- CityGame/Login/LoginViewModel.swift
+++ CityGame/Login/LoginViewModel.swift
@@ -1,6 +1,8 @@
 import Foundation
 import FirebaseAuth
 import StatusAlert
+import Alamofire
+
 
 class LoginViewModel {
     var onLoginButtonTapped: (() -> ())? = nil
@@ -15,11 +17,26 @@ class LoginViewModel {
     func checkIfUserIsValid(email: String, password: String) {
         Auth.auth().signIn(withEmail: email, password: password) {
             (user, error) in
+        
+            //============= GET Request =======================
+
+            let url = URL(string: "http://citygame.hostingasp.pl/api/User?UserName=Stachu&Password=Stachu1@")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) {(response, data, error) in
+                guard let data = data else { return }
+                print(String(data: data, encoding: .utf8)!)
+            }
+            
+            //=================================================
+            
             if error == nil {
                 self.onLoginButtonTapped?()
                 print(user?.user.email)
                 print("login success!")
             } else {
+                print("login success!")
                 self.onError(errorMessage: error?.localizedDescription)
             }
         }
